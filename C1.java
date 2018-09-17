d81e38004096f25f1fe612b620a03e0a35f15b2a
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2018-09-18 00:27:44 +0530

    September Long Challenge 18 Problem 01:

---
+++ C1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class C1{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int cases = scan.nextInt();
+		for(int i=1;i<=cases;i++) {
+				int n = scan.nextInt();
+				int x = scan.nextInt();
+				int s = scan.nextInt();
+				for(int j=1;j<=s;j++) {
+					int a = scan.nextInt();
+					int b = scan.nextInt();
+					if(x == a) {
+						x = b;
+					}else if(x == b) {
+						x = a;
+					}
+				}
+				System.out.println(x);
+		}
+	}
+}
